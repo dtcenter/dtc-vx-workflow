bcea2032d68d04ee6b8107a9a42f741f68a57047
Michael J. Kavulich, Jr. <kavulich@ucar.edu>
2026-01-14 00:31:14 +0000

    Adding Point2Grid task to workflow
    Still some known issues:
     - Only retrieves hour 00 for a given day
     - Does not start at the initial forecast hour, but rather 00z of the first day

---
+++ jobs/POINT2GRID.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+#
+#-----------------------------------------------------------------------
+#
+# This script runs the METplus Point2Grid tool for verification
+#
+# Run-time environment variables:
+#
+#    GLOBAL_VAR_DEFNS_FP
+#
+# Experiment variables
+#
+#  user:
+#    SCRIPTSdir
+#    USHdir
+#
+#-----------------------------------------------------------------------
+#
+
+#
+#-----------------------------------------------------------------------
+#
+# Source the conda setup and variable definitions files
+#
+#-----------------------------------------------------------------------
+#
+. $USHdir/../setup_conda.sh
+. $USHdir/source_util_funcs.sh
+sections=(
+  user
+  workflow
+)
+for sect in ${sections[*]} ; do
+  source_yaml ${GLOBAL_VAR_DEFNS_FP} ${sect}
+done
+#
+#-----------------------------------------------------------------------
+#
+# Get the full path to the file in which this script/function is located 
+# (scrfunc_fp), the name of that file (scrfunc_fn), and the directory in
+# which the file is located (scrfunc_dir).
+#
+#-----------------------------------------------------------------------
+#
+scrfunc_fp=$( $READLINK -f "${BASH_SOURCE[0]}" )
+scrfunc_fn=$( basename "${scrfunc_fp}" )
+scrfunc_dir=$( dirname "${scrfunc_fp}" )
+
+print_info_msg "
+========================================================================
+Entering script:  \"${scrfunc_fn}\"
+In directory:     \"${scrfunc_dir}\"
+========================================================================"
+#
+# Call the run script
+#
+VERBOSE_FLAG=""
+if [ "${VERBOSE}" = "True" ]; then
+  VERBOSE_FLAG="--verbose"
+fi
+python $SCRIPTSdir/point2grid.py ${VERBOSE_FLAG} \
+  --config="${GLOBAL_VAR_DEFNS_FP}" \
+  --cycle_date="${YYMMDD}${HH}" \
+  --field_group="${FIELD_GROUP}" \
+  --fcst_level="${FCST_LEVEL}" \
+  --fcst_thresh="${FCST_THRESH}" \
+  --obtype="${OBTYPE}" \
+  --obs_dir="${OBS_DIR}" || \
+print_err_msg_exit "\
+Call to \"point2grid.py\" from \"${scrfunc_fn}\" failed."
+
