8c8a6809c5601c84a3e02681a17017dc7c1d9221
Michael J. Kavulich, Jr. <kavulich@ucar.edu>
2026-01-30 18:20:27 +0000

    Add RegridDataPlane tool between point2grid and mode to interpolate/regrid data to be more object friendly
    Also, adding GOES QC flags to point2grid process

--- scripts/regriddataplane.py
+++ scripts/regriddataplane.py
@@ -22,8 +22,8 @@ from python_utils.metplus_conf_utils import render_metplus_confs
 from set_leadhrs import set_leadhrs
 from set_vx_params import set_vx_params
 
-def main(config_file,cdate,obs_dir,field_group,obtype,fcst_level,fcst_thresh,verbose):
-    """Main program for setting up Point2Grid task and calling METplus wrapper"""
+def main(config_file,cdate,field_group,obtype,verbose):
+    """Main program for setting up RegridDataPlane task and calling METplus wrapper"""
     lgr = logging.getLogger(__name__)
 
     # Read config settings
@@ -31,11 +31,7 @@ def main(config_file,cdate,obs_dir,field_group,obtype,fcst_level,fcst_thresh,ver
 
     # Set some aliases
     vxcfg = cfg["verification"]
-
-    # Check that basic input directories exist:
-    lgr.info(f"{obs_dir=}")
-    if not Path(obs_dir).is_dir():
-        raise FileNotFoundError(f"OBS_DIR does not exist or is not a directory:\n{obs_dir=}")
+    rdpcfg = cfg['regriddataplane']
 
     # Make a dictionary of variables that may need to be substituted; these will be used to replace
     # bash-like variables in some strings. This is needed to maintain some functionality while we
@@ -48,17 +44,21 @@ def main(config_file,cdate,obs_dir,field_group,obtype,fcst_level,fcst_thresh,ver
     # METplus tool to be run as well as other file/directory parameters.
 
     exptdir=vxcfg["VX_OUTPUT_BASEDIR"]
-    MetplusToolName = "Point2Grid"
+    MetplusToolName = "RegridDataPlane"
     if obtype == "GOESAOD":
-        obs_in_dir = vxcfg["GOESAOD_OBS_DIR"]
-        obs_in_fn_template = vxcfg["OBS_GOESAOD_FN_TEMPLATES"][1]
-        adp_input_fn_template = Path(vxcfg["GOESADP_OBS_DIR"],vxcfg["OBS_GOESADP_FN_TEMPLATES"][1])
-        output_fn_template = f'{vxcfg["OBS_GOES_AOD_FN_TEMPLATE_POINT2GRID_OUTPUT"]}'
+        obs_in_dir = Path(exptdir, cdate, "metprd", "Point2Grid")
+        obs_in_fn_template = vxcfg["OBS_GOES_AOD_FN_TEMPLATE_POINT2GRID_OUTPUT"]
+        output_fn_template = f'regrid_{vxcfg["OBS_GOES_AOD_FN_TEMPLATE_POINT2GRID_OUTPUT"]}'
         # Get the list of all the times in the current day at which to retrieve obs.
     else:
         raise ValueError(f"Invalid OBTYPE for {MetplusToolName}: {obtype}")
 
-    #Point2Grid does not honor "time lag" shifts, so remove from template
+    # Check that basic input directories exist:
+    lgr.info(f"{obs_in_dir=}")
+    if not Path(obs_in_dir).is_dir():
+        raise FileNotFoundError(f"OBS_DIR does not exist or is not a directory:\n{obs_in_dir=}")
+
+    #RegridDataPlane does not honor "time lag" shifts, so remove from template
     fcst_fn_template=Path(
                      vxcfg["VX_FCST_INPUT_BASEDIR"],
                      Template(vxcfg["FCST_SUBDIR_TEMPLATE"]).substitute(subvars),
@@ -84,17 +84,9 @@ def main(config_file,cdate,obs_dir,field_group,obtype,fcst_level,fcst_thresh,ver
     if not vx_leadhr_list:
         raise RuntimeError(f"Call to {slh_string}\nreturned an empty list.")
 
-    vx_mask_files=[]
-    if vxcfg["VX_MASK"]:
-        for mask in vxcfg["VX_MASK"]:
-            if os.path.isfile(maskfile:=f"{cfg['user']['METPLUS_CONF']}/{mask}.poly"):
-                vx_mask_files.append(maskfile)
-            else:
-                vx_mask_files.append(f"{os.environ['MET_INSTALL_DIR']}/share/met/poly/{mask}.poly")
-
     # Set the names of the template METplus configuration file, the resulting rendered conf file,
     # and the METplus log file
-    metplus_config_tmpl_fn="Point2Grid.conf"
+    metplus_config_tmpl_fn="RegridDataPlane.conf"
     metplus_config_fn=f"{MetplusToolName}_{field_group}.conf.0"
     metplus_log_fn=f"metplus.log.{metplus_config_fn[:-7]}_{cdate}.0"
 
@@ -109,28 +101,23 @@ def main(config_file,cdate,obs_dir,field_group,obtype,fcst_level,fcst_thresh,ver
                'cdate': cdate,
                'vx_leadhr_list': ', '.join(map(str,vx_leadhr_list)),
                # Interpolation information
-               'regrid_method': cfg['point2grid']['REGRID_METHOD'],
-               'goes_qc_flags': cfg['point2grid']['GOES_QC_FLAGS'],
+               'regrid_method': rdpcfg['REGRID_METHOD'],
+               'gaussian_dx': rdpcfg['GAUSSIAN_DX'],
+               'gaussian_radius': rdpcfg['GAUSSIAN_RADIUS'],
                # Input and output directory/file information.
                'metplus_config_fn': metplus_config_fn,
                'metplus_log_fn': metplus_log_fn,
                'obs_input_dir': obs_in_dir,
                'obs_input_fn_template': obs_in_fn_template,
-               'adp_input_fn_template': adp_input_fn_template,
                'output_dir': output_dir,
                'output_fn_template': output_fn_template,
                'fcst_fn_template': fcst_fn_template,
                # Field information.
                'obtype': obtype,
-               'metplus_templates_dir': cfg['user']['METPLUS_CONF'],
-               'input_field_group': field_group,
-               'input_level_fcst': fcst_level,
-               'input_thresh_fcst': fcst_thresh,
-               # Rest of settings from yaml file
-               'vx_config_dict': vx_config_dict
+               'level': 'L550'
                }
 
-    numprocs=vxcfg['VX_TASKS']
+    numprocs=rdpcfg['TASKS']
     conf_files = render_metplus_confs(cfg,settings,metplus_config_tmpl_fn,vx_leadhr_list,numprocs)
     lgr.debug(f"{conf_files=}")
 
@@ -187,7 +174,7 @@ def setup_logging(debug=False):
 if __name__ == "__main__":
     #Parse arguments
     parser = argparse.ArgumentParser(
-                     description="exscript for running METplus Point2Grid tasks"\
+                     description="exscript for running METplus RegridDataPlane tasks"\
                      "for deterministic verification\n")
 
 #    parser.add_argument('-c', '--config', default='config.yaml',
@@ -198,14 +185,8 @@ if __name__ == "__main__":
                         help='Eight-digit cycle date (YYMMDDHH)')
     parser.add_argument('--field_group', required=True, type=str,
                         help='Group of fields for this verification task (e.g. APCP, REFC, SFC, etc.)')
-    parser.add_argument('--fcst_level', required=True, type=str,
-                        help='The "level" of the observation type as expected by MET (e.g. L0, A03, etc.)')
-    parser.add_argument('--fcst_thresh', required=True, type=str,
-                        help='The set of forecast thresholds to verify against. Valid options are "all" and "none".')
     parser.add_argument('--obtype', required=True, type=str,
                         help='Observation type for this verification task (e.g. NOHRSC, CCPA, NDAS, etc.)')
-    parser.add_argument('--obs_dir', required=True, type=str,
-                        help='Observation directory for this obtype')
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='Script will be run in verbose mode')
     pargs = parser.parse_args()
@@ -216,12 +197,11 @@ if __name__ == "__main__":
         ========================================================================
         Executing program: {__file__}
 
-        This is the ex-script for the task that runs the METplus Point2Grid
+        This is the ex-script for the task that runs the METplus RegridDataPlane
         tool to perform deterministic verification of the specified field group
         (FIELD_GROUP) for a single forecast.
         ========================================================================"""))
 
-    # Retrieve needed args from environment; should pass these explicitly in the future
-    logging.info(f"{os.environ['METPLUS_ROOT']=}")
+    logging.debug(f"{os.environ['METPLUS_ROOT']=}")
 
-    main(pargs.config,pargs.cycle_date,pargs.obs_dir,pargs.field_group,pargs.obtype,pargs.fcst_level,pargs.fcst_thresh,pargs.verbose)
+    main(pargs.config,pargs.cycle_date,pargs.field_group,pargs.obtype,pargs.verbose)
