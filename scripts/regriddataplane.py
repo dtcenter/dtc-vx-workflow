44fc9ea247d402dc7eebcb16f42826e0d437db5a
Michael Kavulich, Jr. <kavulich@ucar.edu>
2026-02-13 16:33:02 +0000

    Replace sys.path.insert calls with setting PYTHONPATH env variable to be more compliant with PEP8 standards

--- scripts/regriddataplane.py
+++ scripts/regriddataplane.py
@@ -15,8 +15,6 @@ from jinja2 import Environment, FileSystemLoader
 
 import uwtools.api.config as uwconfig
 
-sys.path.insert(1, os.environ['USHdir'])
-
 from eval_metplus_timestr_tmpl import eval_metplus_timestr_tmpl
 from python_utils import setup_logging,render_metplus_confs
 from set_leadhrs import set_leadhrs
