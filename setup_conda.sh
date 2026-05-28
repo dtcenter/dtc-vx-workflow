894de828a21917783d6ebc24c6b6ae1cc80ebb0f
Michael Kavulich, Jr. <kavulich@ucar.edu>
2026-05-28 00:17:17 +0000

    Fix setup_conda for environments where LD_LIBRARY_PATH is unset

--- setup_conda.sh
+++ setup_conda.sh
@@ -29,7 +29,9 @@ fi
 if [[ ! "$PATH" =~ "$CONDA_BUILD_DIR" ]]; then
   export PATH=${CONDA_BUILD_DIR}/condabin:${CONDA_BUILD_DIR}/bin:${PATH}
 fi
-if [[ ! "$LD_LIBRARY_PATH" =~ "$CONDA_BUILD_DIR" ]]; then
+if [[ -z "${LD_LIBRARY_PATH:-}" ]]; then
+  export LD_LIBRARY_PATH=${CONDA_BUILD_DIR}/lib
+elif [[ ! "${LD_LIBRARY_PATH}" =~ "$CONDA_BUILD_DIR" ]]; then
   export LD_LIBRARY_PATH=${CONDA_BUILD_DIR}/lib:${LD_LIBRARY_PATH}
 fi
 
